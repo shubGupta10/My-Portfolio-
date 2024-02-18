1fa318cdf4bf7093d6ba0e38411bf4d30981f8ed
Shubham Kumar Gupta <shubhamkumargupta1012@gmail.com>
2024-02-19 00:28:10 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,17 @@
+const about = document.getElementById('about');
+const aboutbtn = document.getElementById('aboutBtn');
+
+aboutbtn.addEventListener('click', function(){
+    console.log("click");
+    about.scrollIntoView();
+})
+
+const skills = document.getElementById('skills');
+const skillsBtn = document.getElementById('skillsBtn');
+const skillsContainer = document.getElementById('skillsSection');
+
+skillsBtn.addEventListener('click', function(){
+    console.log("skills button working");
+    skillsContainer.scrollIntoView({ behavior: 'smooth', block: 'start' });
+});
+
