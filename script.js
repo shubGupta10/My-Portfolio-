5e01202a1586524362afe62b02ad443d88e5a9b4
Shubham Kumar Gupta <shubhamkumargupta1012@gmail.com>
2024-04-03 19:43:55 +0530

    Added Animations

--- script.js
+++ script.js
@@ -64,3 +64,20 @@ contact.addEventListener('click', function(event){
 })
 
 
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry)=> {
+        console.log(entry)
+        if(entry.isIntersecting){
+            entry.target.classList.add('show');
+        } else{
+            entry.target.classList.remove('show')
+        }
+    })
+});
+
+
+const hiddenElements = document.querySelectorAll('.goodlook');
+hiddenElements.forEach((el) => observer.observe(el));
+
+
+
