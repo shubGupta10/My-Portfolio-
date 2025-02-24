1d22868f561b2809832daf9149ac3935bb3fa2d2
Shubham Kumar Gupta <132940559+shubGupta10@users.noreply.github.com>
2025-02-24 16:43:29 +0530

    Update utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,6 +5,7 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
+
 export function formatDate(date: string) {
   let currentDate = new Date().getTime();
   if (!date.includes("T")) {
