a4dba2c69867b39c9d68c1500e39bf2776d1780c
Shubham Kumar Gupta <shubhamkumargupta1012@gmail.com>
2025-11-28 12:53:24 +0530

    added / in login

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -53,7 +53,7 @@ function Navbar({ menuOpen, setMenuOpen }) {
 
                     {/* Logo */}
                     <Link
-                        href={isLoginPage ? "/" : "#home"}
+                        href={isLoginPage ? "/" : "/"}
                         className="text-xl font-semibold tracking-wide bg-gradient-to-r from-blue-400 to-cyan-300 bg-clip-text text-transparent hover:opacity-90 transition-all cursor-pointer"
                     >
                         SHUBHAM
