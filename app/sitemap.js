ef4e2a0e7a4cfc936f065d68e86e0e7a5824cc6c
Shubham Kumar Gupta <shubhamkumargupta1012@gmail.com>
2025-12-02 11:57:47 +0530

    added sitemaps

--- app/sitemap.js
+++ app/sitemap.js
@@ -2,22 +2,23 @@ import { fetchPublishedBlogs } from "@/lib/actions";
 
 export default async function sitemap() {
   const baseUrl = "https://www.shubhamgupta.online";
+  const today = new Date().toISOString().split("T")[0]; 
 
   const blogs = await fetchPublishedBlogs();
 
   const blogUrls = blogs.map((blog) => ({
     url: `${baseUrl}/blog/${blog.slug}`,
-    lastModified: blog.updated_at || blog.created_at,
+    lastModified: (blog.updated_at || blog.created_at).split("T")[0], 
   }));
 
   return [
     {
       url: baseUrl,
-      lastModified: new Date().toISOString(),
+      lastModified: today,
     },
     {
       url: `${baseUrl}/blog`,
-      lastModified: new Date().toISOString(),
+      lastModified: today,
     },
     ...blogUrls,
   ];
